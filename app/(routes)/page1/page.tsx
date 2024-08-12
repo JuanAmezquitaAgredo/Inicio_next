5a3aae02abc30bf85e9fffb593ce905c877d528e
JuanAmezquitaAgredo <jagredo03@gmail.com>
2024-08-12 07:50:57 -0500

    first_init

---
+++ app/(routes)/page1/page.tsx
@@ -0,0 +1,14 @@
+
+const Page1 = () => {
+  return (
+    <div>
+      <h1>Página 1</h1>
+      <br />
+      <p>Bienvenido a la Página 1</p>
+      <br />
+      <a href="/">Ir a la Página principal</a>
+    </div>
+  );
+};
+
+export default Page1;
---
+++ app/(routes)/page2/page.tsx
@@ -0,0 +1,14 @@
+
+const Page2 = () => {
+  return (
+    <div>
+      <h1>Página 2</h1>
+      <br />
+      <p>Bienvenido a la Página 2</p>
+      <br />
+      <a href="/">Ir a la Página principal</a>
+    </div>
+  );
+};
+
+export default Page2;
